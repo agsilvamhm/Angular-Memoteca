dc2c0c15025b4be7d535124271a52fe12cc8cb39
Adalberto Gonçalves da Silva <adalberto.goncalves.silva@gmail.com>
2023-03-13 20:59:05 -0300

    Registrando as aulas de directivas

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -9,15 +9,25 @@ export class ListarPensamentoComponent implements OnInit {
 
   listaPensamentos=[
     {
-    conteudo: 'Passo informações para o componente filho',
-    autoria: 'Componente pai',
-    modelo: 'modelo2'
-    },
-    {
-      conteudo: 'Minha propriedade é decorada com @Input()',
-      autoria: 'Componente filho',
-      modelo: 'modelo3'
-      }
+      conteudo: 'Passo informações para o componente filho',
+      autoria: 'Componente pai',
+      modelo: 'modelo2'
+      },
+      {
+        conteudo: 'Minha propriedade é decorada com @Input()',
+        autoria: 'Componente filho',
+        modelo: 'modelo3'
+        },
+        {
+          conteudo: 'Vigie seus pensamentos, pois eles se tornam palavras. Vigie suas palavras, pois elas se tornam ações. Cuidado com suas ações, porque elas se tornam hábito. Cuidado com seus hábitos, porque eles se tornam seu caráter.',
+          autoria: 'Componente filho',
+          modelo: 'modelo3'
+          },
+          {
+            conteudo: 'Extenso é o pensamento, imensurável sentimento da dor que cobre o mundo e você diz que não ta vendo, espero mil respostas as feridas estão expostas, o ser humano no abandono a juventude vira as costas reflexão cruel, o gosto amargo do fel, a alienação por aqui passou e fez o seu papel, perdidos sem perspectiva a humanidade assim caminha a beira do abismo ignorando a auto-estima',
+            autoria: 'Componente filho',
+            modelo: 'modelo3'
+            },
   ]
 
   constructor() { }
