6f8e1f571fe7b89316c7f3d7f10ceff5556ace95
Adalberto Gonçalves da Silva <adalberto.goncalves.silva@gmail.com>
2023-03-18 19:10:03 -0300

    Registrando a alteração nos pensamentos

--- src/app/componentes/pensamentos/excluir-pensamento/excluir-pensamento.component.ts
+++ src/app/componentes/pensamentos/excluir-pensamento/excluir-pensamento.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
 
 @Component({
   selector: 'app-excluir-pensamento',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExcluirPensamentoComponent implements OnInit {
 
-  constructor() { }
+  pensamento: Pensamento ={
+    id: 0,
+    conteudo: '',
+    autoria: '',
+    modelo: ''
+  }
+
+  constructor(
+    private service: PensamentoService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    this.service.buscarPorId(parseInt(id!)).subscribe((pensamento) => {
+      this.pensamento = pensamento
+    })
+  }
+
+  excluirPensamento(){
+    if (this.pensamento.id){
+      this.service.excluir(this.pensamento.id).subscribe(() => {
+        this.router.navigate(['/listarPensamento'])
+      })
+    }
+  }
+
+  cancelar(){
+    this.router.navigate(['/listarPensamento'])
   }
 
 }
