794809c34034cf51ae4002f36ff36b19a4c45500
Adalberto Gonçalves da Silva <adalberto.goncalves.silva@gmail.com>
2023-03-19 04:22:20 -0300

    Registrando a aula de validações dos formulários

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -2,7 +2,7 @@ import { PensamentoService } from './../pensamento.service';
 import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -11,12 +11,6 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./criar-pensamento.component.css']
 })
 export class CriarPensamentoComponent implements OnInit {
-  pensamento : Pensamento = {
-    id :  0,
-    conteudo : '',
-    autoria : '',
-    modelo : ''
-  }
 
   formulario!: FormGroup
 
@@ -26,16 +20,22 @@ export class CriarPensamentoComponent implements OnInit {
 
   ngOnInit(): void {
     this.formulario = this.formBuilder.group({
-      conteudo: ['Fomulário reativo'],
-      autoria: [''],
+      conteudo: ['', Validators.compose(
+                    [Validators.required,
+                     Validators.pattern(/(.|\s)*\S(.|\s)*/)])],
+      autoria: ['', Validators.compose([Validators.required,
+                     Validators.minLength(3)])],
       modelo: ['modelo1']
     })
   }
 
   criarPensamento(){
-    this.service.criar(this.pensamento).subscribe(() => {
-      this.router.navigate(['/listarPensamento'])
-    })
+    console.log(this.formulario.get('autoria')?.errors)
+    if (this.formulario.valid){
+      this.service.criar(this.formulario.value).subscribe(() => {
+        this.router.navigate(['/listarPensamento'])
+      })
+    }
   }
 
   cancelarPensamento(){
